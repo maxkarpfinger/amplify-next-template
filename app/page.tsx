8d3edd537bd57ce84d6784b9383ff0305dff9764
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2025-04-26 09:34:20 +0200

    revert set tab title and remove year from date labels

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function App() {
   const [preferredDates, setPreferredDates] = useState<string[]>([]);
 
   const dates = [
-    { value: "2025-07-18", label: "18. Juli 2025" },
-    { value: "2025-07-19", label: "19. Juli 2025" },
-    { value: "2025-08-01", label: "01. August 2025" },
-    { value: "2025-08-02", label: "02. August 2025" },
+    { value: "2025-07-18", label: "18. Juli" },
+    { value: "2025-07-19", label: "19. Juli" },
+    { value: "2025-08-01", label: "01. August" },
+    { value: "2025-08-02", label: "02. August" },
   ];
 
   function handleDateChange(event: React.ChangeEvent<HTMLInputElement>) {
